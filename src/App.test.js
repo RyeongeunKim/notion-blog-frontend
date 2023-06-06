a7e5b0fc79cb0286f0f600447248ab396cd652b5
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-06-06 09:40:49 +0900

    router 테스트 코드 추가

---
+++ src/App.test.js
@@ -0,0 +1,24 @@
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import routesConfig from "./routes/routesConfig";
+
+describe("App component", () => {
+  test("최초 페이지는 / 경로이다.", async () => {
+    const router = createMemoryRouter(routesConfig, {
+      initialEntries: ["/"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    // make assertions, await changes, etc... callback 호출
+  });
+  test("Posts 메뉴를 클릭하면 /posts/:postId 경로로 이동한다", async () => {
+    const router = createMemoryRouter(routesConfig, {
+      initialEntries: ["/posts"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    // make assertions, await changes, etc... callback 호출
+  });
+});
