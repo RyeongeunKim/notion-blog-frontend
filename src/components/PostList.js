f01adf54bd3fb817097c8ecb2dddba2f046060e3
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-06-06 17:53:18 +0900

    테스트 코드 삭제

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,32 +1,16 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./PostList.module.css";
 
-function PostList(props) {
-  const [db, setDB] = useState({});
-  const clickHandler = async () => {
-    let database_id = process.env.REACT_APP_NOTION_DATABASE_ID;
-    fetch("http://localhost:3002/" + database_id).then(async (resp) => {
-      console.log(resp);
-      setDB(await resp.json());
-    });
-  };
-
-  useEffect(() => {
-    console.log(db);
-  }, [db]);
-
+function PostList({ posts }) {
   return (
     <div>
-      <button onClick={clickHandler}>테스트버튼</button>
       <ul className={classes.list}>
-        <li>
-          <Link to={`0`}>글 제목 (1)</Link>
-        </li>
-        <li>
-          <Link to={`1`}>글 제목 (2)</Link>
-        </li>
+        {posts.map((post, index) => (
+          <li key={post.id}>
+            <Link to={`${post.id}`}>{posts[index].properties["이름"].title[0].plain_text}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
