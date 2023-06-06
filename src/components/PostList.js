72c3da2ac550397f97a9957ce1d451bcc8e2d9f1
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-06-06 15:51:04 +0900

    노션 api 호출 코드 작성

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./PostList.module.css";
 
 function PostList(props) {
+  const [db, setDB] = useState({});
+  const clickHandler = async () => {
+    let database_id = process.env.REACT_APP_NOTION_DATABASE_ID;
+    fetch("http://localhost:3002/" + database_id).then(async (resp) => {
+      console.log(resp);
+      setDB(await resp.json());
+    });
+  };
+
+  useEffect(() => {
+    console.log(db);
+  }, [db]);
+
   return (
     <div>
+      <button onClick={clickHandler}>테스트버튼</button>
       <ul className={classes.list}>
         <li>
           <Link to={`0`}>글 제목 (1)</Link>
