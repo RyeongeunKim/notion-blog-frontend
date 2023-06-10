59495004183e6dd00a237d0beaafaa4b5b66395a
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-06-11 00:32:23 +0900

    주석 제거

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import RootLayout from "./pages/Root";
-import HomePage from "./pages/Home";
-import PostsRootLayout from "./pages/PostsRoot";
-import PostsPage from "./pages/Posts";
-import PostDetailPage from "./pages/PostDetail";
-
 import routesConfig from "./routes/routesConfig";
 
 const router = createBrowserRouter(routesConfig);
