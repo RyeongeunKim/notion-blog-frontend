52759863bd598846bef9b45b51727d6e46c44b7f
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-06-10 17:46:01 +0900

    글 목록 request 테스트 코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -14,41 +14,41 @@ describe("App component", () => {
     const outputElement = screen.getByText("HomePage", { exact: false });
     expect(outputElement).toBeInTheDocument();
   });
-  test("Posts(GNB) 메뉴를 클릭하면 /posts 경로로 이동하고 All Posts(LNB) 메뉴가 렌더링된다.", async () => {
+  // test("Posts(GNB) 메뉴를 클릭하면 /posts 경로로 이동하고 All Posts(LNB) 메뉴가 렌더링된다.", async () => {
+  //   const router = createMemoryRouter(routesConfig, {
+  //     initialEntries: ["/posts"],
+  //   });
+
+  //   render(<RouterProvider router={router} />);
+
+  //   const outputElement = screen.getByText("All Posts", { exact: false });
+  //   expect(outputElement).toBeInTheDocument();
+
+  //   // make assertions, await changes, etc... callback 호출
+  // });
+  test("/posts 에서 request가 성공하면 글 목록이 표시된다.", async () => {
     const router = createMemoryRouter(routesConfig, {
       initialEntries: ["/posts"],
     });
 
     render(<RouterProvider router={router} />);
 
-    const outputElement = screen.getByText("All Posts", { exact: false });
-    expect(outputElement).toBeInTheDocument();
-
-    // make assertions, await changes, etc... callback 호출
+    const listItemElements = await screen.findAllByRole("listitem");
+    expect(listItemElements).not.toHaveLength(0);
   });
-  test("/posts 에서 글 목록이 표시된다.", async () => {
-    const router = createMemoryRouter(routesConfig, {
-      initialEntries: ["/posts"],
-    });
+  // test("/posts 에서 글 제목 (1) 클릭 시 /posts/0 경로로 이동하고 index 0 번이 렌더링 된다.", async () => {
+  //   const router = createMemoryRouter(routesConfig, {
+  //     initialEntries: ["/posts"],
+  //   });
 
-    render(<RouterProvider router={router} />);
+  //   render(<RouterProvider router={router} />);
 
-    const outputElement = screen.getAllByText("글", { exact: false });
-    expect(outputElement[0]).toBeInTheDocument();
-  });
-  test("/posts 에서 글 제목 (1) 클릭 시 /posts/0 경로로 이동하고 index 0 번이 렌더링 된다.", async () => {
-    const router = createMemoryRouter(routesConfig, {
-      initialEntries: ["/posts"],
-    });
+  //   const linkElement = screen.getByText("글 제목 (1)", { exact: true });
+  //   userEvent.click(linkElement);
 
-    render(<RouterProvider router={router} />);
-
-    const linkElement = screen.getByText("글 제목 (1)", { exact: true });
-    userEvent.click(linkElement);
-
-    const outputElement = screen.getByText("0", {
-      exact: false,
-    });
-    expect(outputElement).toBeInTheDocument();
-  });
+  //   const outputElement = screen.getByText("0", {
+  //     exact: false,
+  //   });
+  //   expect(outputElement).toBeInTheDocument();
+  // });
 });
