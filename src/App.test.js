698018775f2dc74973d462bbfeb840f210fdcd5a
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-06-12 22:54:52 +0900

    테스트 작은 단위로 분리

--- src/App.test.js
+++ src/App.test.js
@@ -35,39 +35,57 @@ describe("App component", () => {
 
     expect(postsLinkElement).toBeInTheDocument();
   });
-  test("Posts 메뉴를 클릭하면 /posts 경로로 이동하고 All Posts(LNB) 메뉴가 렌더링된다.", async () => {
-    const router = createMemoryRouter(routesConfig, {
-      initialEntries: ["/posts"],
-    });
+  test("/ 에서 Posts 메뉴를 클릭하면 /posts 경로로 이동한다.", async () => {
+    const { router } = setupRouter("/");
 
-    render(<RouterProvider router={router} />);
+    const linkElement = await screen.findByRole("link", { name: "Posts" });
+    userEvent.click(linkElement);
+
+    await waitFor(() => {
+      expect(router.state.location.pathname).toEqual("/posts");
+    });
+  });
+  test("/posts 에서 All Posts(LNB) 메뉴가 렌더링된다.", async () => {
+    setupRouter("/posts");
 
     const linkElement = await screen.findByRole("link", { name: "All Posts" });
     expect(linkElement).toBeInTheDocument();
   });
   test("/posts 에서 request가 성공하면 글 목록이 표시된다.", async () => {
-    const router = createMemoryRouter(routesConfig, {
-      initialEntries: ["/posts"],
-    });
-
-    render(<RouterProvider router={router} />);
+    setupRouter("/posts");
 
     const listItemElements = await screen.findAllByRole("listitem");
     expect(listItemElements).not.toHaveLength(0);
   });
-  test("/posts 에서 글 제목 (1) 클릭하면 /posts/:postId 경로로 이동하고 선택한 글 제목 및 내용이 렌더링 된다.", async () => {
-    const router = createMemoryRouter(routesConfig, {
-      initialEntries: ["/posts"],
-    });
-
-    render(<RouterProvider router={router} />);
+  test("/posts 에서 글 제목을 클릭하면 /posts/:postId 경로로 이동한다.", async () => {
+    const { router } = setupRouter("/posts");
 
     const linkElement = await screen.findByRole(
       "link",
       {
         name: "글 제목 (1)",
       },
-      { timeout: 8000 }
+      { timeout: 5000 }
+    );
+
+    userEvent.click(linkElement);
+
+    await waitFor(() => {
+      const pathName = router.state.location.pathname;
+      const splitPath = pathName.split("/");
+      const postId = splitPath.length > 2 ? splitPath[2] : "";
+      expect(pathName).toEqual(`/posts/${postId}`);
+    });
+  });
+  test("/posts/:postId 에서 선택한 글 제목 및 내용이 렌더링 된다.", async () => {
+    setupRouter("/posts");
+
+    const linkElement = await screen.findByRole(
+      "link",
+      {
+        name: "글 제목 (1)",
+      },
+      { timeout: 5000 }
     );
 
     userEvent.click(linkElement);
@@ -78,7 +96,7 @@ describe("App component", () => {
     const outputPageContentsElement = await screen.findAllByText(
       "내용",
       { exact: false },
-      { timeout: 8000 }
+      { timeout: 5000 }
     );
     expect(outputPageContentsElement).not.toHaveLength(0);
   });
