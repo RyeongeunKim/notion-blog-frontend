94f8ef6f0df80f722074dbfe3ccccd3e67fa5363
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-06-18 18:53:09 +0900

    postItem 주석 제거

--- src/components/Posts/PostItem.js
+++ src/components/Posts/PostItem.js
@@ -7,39 +7,6 @@ function PostItem({ blocks }) {
     state: { title },
   } = useLocation();
 
-  // const [contents, setContents] = useState([]);
-
-  // useEffect(() => {
-  //   let contents = [];
-
-  //   if (blocks.length) {
-  //     // console.log("blocks = ", blocks);
-  //     blocks.forEach((item) => {
-  //       let id = item.id;
-  //       let richTexts = [];
-  //       let plainText = "";
-  //       let content;
-
-  //       if (item.type === "paragraph") {
-  //         richTexts = item.paragraph.rich_text;
-  //       } else if (item.type === "numbered_list_item") {
-  //         richTexts = item.numbered_list_item.rich_text;
-  //       }
-
-  //       if (richTexts.length) {
-  //         plainText = richTexts[0].plain_text;
-  //       }
-
-  //       if (plainText) {
-  //         content = { id, plainText };
-  //         contents.push(content);
-  //       }
-  //     });
-  //   }
-
-  //   setContents(contents);
-  // }, []);
-
   return (
     <div>
       <h1>{title}</h1>
